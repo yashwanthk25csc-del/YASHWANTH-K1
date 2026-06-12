8f35133cf6755680d98b8449edadf3916cb01dd4
STUDENT <yashwanthk25csc@vetias.ac.in>
2026-06-12 11:41:15 +0530

    added operators.js

--- operators.js
+++ operators.js
@@ -6,7 +6,15 @@ console.log(a%b)
 console.log(a/b)
 console.log(a**b)
 c=20
-console.log(a+=b)
-console.log(a-=b)
-console.log(a%=b)
-console.log(a*=b)
+console.log(a+=c)
+console.log(a-=c)
+console.log(a%=c)
+console.log(a*=c)
+var a=10
+var b=30
+console.log("comparision operator")
+console.log(a==b)
+console.log(a!=b)
+console.log(a<=b)
+console.log(a>=b)
+
