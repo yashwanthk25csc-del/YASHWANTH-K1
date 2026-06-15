0320683fd0dfff89f7423e994083358a81bf0494
STUDENT <yashwanthk25csc@vetias.ac.in>
2026-06-15 11:09:10 +0530

    addedswitch statement

---
+++ switch statement.js
@@ -0,0 +1,16 @@
+var signal = "white"
+switch (signal){
+    case "green":
+    console.log("Go!");
+    break;
+     case "yellow"
+     console.log("wait!");
+     break;
+     case "red"
+     console.log("stop!");
+     break;
+     default:
+        console.log("invalid signal");
+
+
+}
